af6204f7c773aa059bde840201204dd0b34689ba
Ioana Iaru <iaruioana@gmail.com>
2019-03-07 12:01:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import TWEPI
 
-consumer_key = "NFtekK2vZgidWuiHoKtEePPAE"
-consumer_secret = "cQuUBs0GXFWGNnHqJcpT6iaxZnnCya1kDtW3lbLiDWw4qZwSle"
-access_token = "1043469883118370818-wfbLujkappWWwVR0FESI3M1wP2F9om"
-access_token_secret = "sSIg2rrh6SID4GCzgs9Sy14TKdIj3qzfb36ZrFPhPXE7l"
-botometer_key = "9a5443c1c9msh18ac51b2e771bb1p1b6347jsn2d8a54d809e9"
-
 api = TWEPI.TwePI(consumer_key, consumer_secret, access_token, access_token_secret, botometer_key)
 
 # TEST HERE
