89c6cab4621fc2b813972ab38f589832728164ba
Daniel O'Connor <DanielOC004@gmail.com>
2019-03-14 17:06:11 +0000

    Add flask to website

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import *
+import sys
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return redirect(url_for('index', code=302))
+
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/page2', methods=['POST', 'GET'])
+def second():
+    if request.method == 'POST':
+        hashtag = request.form['textarea1']
+        start_date = request.form['start-date']
+        end_date = request.form['end-date']
+        text = "Gathered tweets from " + start_date + " to " + end_date + " for #" + hashtag
+        return render_template('page2.html', title="Analysis", return_text=text)
+    elif request.method == 'GET':
+        return redirect(url_for('index', code=302))
---
+++ static/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+    $(document).ready(function(){
+      $('.datepicker').datepicker();
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
