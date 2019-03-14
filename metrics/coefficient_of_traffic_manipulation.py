8f8555e42e837453a9035bde66a337fc450699c7
Ioana Iaru <iaruioana@gmail.com>
2019-03-15 00:43:51 +0200

    Coefficient of Traffic Manipulation metrics

---
+++ metrics/coefficient_of_traffic_manipulation.py
@@ -0,0 +1,119 @@
+import tweepy
+import collections
+
+"""
+Returns percentage of retweets in a list of tweets.
+Raises error if list is empty.
+
+Args:
+    tweets (list of Tweet objects) -- the list of tweets.
+
+Returns:
+    float -- the percentage of retweets in 'tweets', a floating-point number between 0 and 100.
+"""
+def retweet_proportion(tweets):
+    retweets = 0
+    original = 0
+    
+    for tweet in tweets:
+        if(hasattr(tweet, "retweeted_status")):
+            retweets += 1
+        else:
+            original += 1
+
+    try:
+        proportion = retweets/(retweets+original)
+        return proportion*100
+        
+    except ZeroDivisionError:
+        print("No tweets found.")
+        
+
+"""
+Return a Counter of all the distinct users in a given list of tweets,
+together with the number of tweets from each user.
+
+Args:
+    tweets (list of Tweet objects) -- the list of tweets.
+
+Returns:
+    Counter -- a Counter of user IDs (str)
+            consisting of the authors of the tweets in 'tweets'
+            and the number of tweets in 'tweets' made by each of them.
+"""
+
+def _users_with_frequency(tweets):
+    
+    users = list(map(lambda tweet: tweet.user.id_str, tweets))
+    users_with_freq = collections.Counter(users)
+    
+    return users_with_freq
+
+
+"""
+Return the average number of tweets per user from a list of tweets.
+
+Args:
+    tweets (list of Tweet objects) -- the list of tweets.
+
+Returns:
+    float -- average number of tweets per user
+"""
+
+def average_tweets_per_user(tweets):
+    
+    tweets_number = len(tweets)
+    users = _users_with_frequency(tweets)
+    users_number = len(users)
+
+    return tweets_number/users_number
+
+
+"""
+Return the percentage of the traffic that comes from the n most active accounts in a list of tweets.
+
+Args:
+    tweets (list of Tweet objects) -- the list of tweets.
+    n (int) -- number of most active users to be considered.
+
+Returns:
+    float -- percentage of the traffic that comes from the top n most active users in 'tweets',
+            as a floating-point number between 0 and 100.
+
+"""
+
+def proportion_of_traffic_from_top_users(tweets, n):
+    
+    total_traffic = len(tweets)
+    users_with_freq = _users_with_frequency(tweets)
+    top_users = users_with_freq.most_common(n)
+    
+    traffic_from_top_users = 0
+    
+    for (user, freq) in top_users:
+        traffic_from_top_users += freq
+        
+    return (traffic_from_top_users/total_traffic)*100
+
+
+"""
+Return the coefficient of traffic manipulation of a topic
+as described in Ben Nimmo's "Measuring Traffic Manipulation on Twitter,
+based on a list of tweets that mention that topic.
+
+Args:
+    tweets (iterable of Tweet objects) -- the list of tweets.
+
+Returns:
+    float -- the coefficient of traffic manipulation.
+"""
+
+def coefficient(tweets):
+    
+    tweets = list(tweets)
+    
+    r = retweet_proportion(tweets)
+    f = proportion_of_traffic_from_top_users(tweets, 50)
+    u = average_tweets_per_user(tweets)
+    
+    return (r/10 + f + u)
