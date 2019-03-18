8dc5ad9c6c89838972f102519a9be718ebdf2c09
AndreiDiaconu1 <acdiaconu98@gmail.com>
2019-03-18 22:52:39 +0200

    Deleted useless file

--- metrics/CreateDBFromArchive/test.py
+++
@@ -1,9 +0,0 @@
-import ArchiveToDBController as ATDBC
-import json
-
-keys = open("../../keys.json")
-data = json.load(keys)
-
-absolute_path_to_archive = data["archive_path"]
-runner = ATDBC.ArchiveToDBController(absolute_path_to_archive)
-runner.create_db()
