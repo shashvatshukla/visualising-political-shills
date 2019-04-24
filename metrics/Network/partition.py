0f394e3c0ad7eed0e2a96c64012285eeec958bde
Daniel O'Connor <DanielOC004@gmail.com>
2019-04-24 16:39:28 +0100

    Add Fiedler partition

---
+++ metrics/Network/partition.py
@@ -0,0 +1,71 @@
+import psycopg2
+import consts
+import numpy as np
+from scipy.sparse import coo_matrix
+from scipy.sparse.linalg import eigs
+
+connection = psycopg2.connect(**consts.db_creds)
+
+
+def get_users():
+    cursor = connection.cursor()
+    select_users = ''' SELECT usr, other_usr
+                       FROM influences'''
+    cursor.execute(select_users)
+    users = set()
+    fetched = [None]
+    while len(fetched) > 0:
+        fetched = cursor.fetchall()
+        for record in fetched:
+            users.add(record[0])
+            users.add(record[1])
+    return list(users)
+
+
+def get_incidence_matrix(users_list):
+    users_dict = {}
+    for i, user in enumerate(users_list):
+        users_dict[user] = i
+    cursor = connection.cursor()
+    get_size = ''' SELECT COUNT(*)
+                   FROM influences;'''
+    cursor.execute(get_size)
+    num_edges = cursor.fetchall()[0]
+    select_influence = ''' SELECT usr, other_usr
+                           FROM influences'''
+    cursor.execute(select_influence)
+    data = []
+    i = []
+    j = []
+    fetched = [None]
+    count = 0
+    while len(fetched) > 0:
+        fetched = cursor.fetchall()
+        for edge in fetched:
+            vertices = (users_dict[edge[0]], users_dict[edge[1]])
+            if vertices[0] == vertices[1]:
+                continue
+            data.extend([-1., 1.])
+            i.extend([count, count])
+            j.extend([min(*vertices), max(*vertices)])
+            count += 1
+    return coo_matrix((data, (i, j)), shape = (int(num_edges[0]), len(users_list)))
+
+
+def get_partition(users_list):
+    incidence = get_incidence_matrix(users_list)
+    laplacian = incidence.transpose()*incidence
+    w, v = eigs(laplacian)
+    inds = np.argsort(np.real(w))
+    partition = []
+    for i in v[:, inds[1]]:
+        if i > 0:
+            partition.append(True)
+        else:
+            partition.append(False)
+    return partition
+
+
+get_partition(sorted(get_users()))
+
+
