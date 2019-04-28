39f3010b63bbd7be9efaadd067b193616e941f18
Daniel O'Connor <DanielOC004@gmail.com>
2019-04-28 12:59:18 +0100

    Add partitioning humans and bots

--- metrics/Network/partition.py
+++ metrics/Network/partition.py
@@ -1,13 +1,17 @@
 import psycopg2
 import consts
 import numpy as np
+import tweepy
+from metrics.api_for_search import ShillSearchAPI
 from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import eigs
+from metrics.BotDetection.logistic_regression import classify
+from metrics.BotDetection.helper_functions import get_record_from_dict
 
 connection = psycopg2.connect(**consts.db_creds)
 
 
-def get_users():
+def get_users(keywords=None):
     cursor = connection.cursor()
     select_users = ''' SELECT usr, other_usr
                        FROM influences'''
@@ -19,7 +23,23 @@ def get_users():
         for record in fetched:
             users.add(record[0])
             users.add(record[1])
-    return list(users)
+    if keywords is not None:
+        cursor = connection.cursor()
+        select_tweets = ''' SELECT usr, text
+                            FROM tweets'''
+        cursor.execute(select_tweets)
+        users_keywords = set()
+        fetched = [None]
+        while len(fetched) > 0:
+            fetched = cursor.fetchall()
+            for record in fetched:
+                user = record[0]
+                tweet = record[1]
+                if user in users and min([(word in tweet) for word in keywords]):
+                    users_keywords.add(user)
+        return list(users_keywords)
+    else:
+        return list(users)
 
 
 def get_incidence_matrix(users_list):
@@ -42,17 +62,18 @@ def get_incidence_matrix(users_list):
     while len(fetched) > 0:
         fetched = cursor.fetchall()
         for edge in fetched:
-            vertices = (users_dict[edge[0]], users_dict[edge[1]])
-            if vertices[0] == vertices[1]:
-                continue
-            data.extend([-1., 1.])
-            i.extend([count, count])
-            j.extend([min(*vertices), max(*vertices)])
-            count += 1
+            if edge[0] in users_dict and edge[1] in users_dict:
+                vertices = (users_dict[edge[0]], users_dict[edge[1]])
+                if vertices[0] == vertices[1]:
+                    continue
+                data.extend([-1., 1.])
+                i.extend([count, count])
+                j.extend([min(*vertices), max(*vertices)])
+                count += 1
     return coo_matrix((data, (i, j)), shape = (int(num_edges[0]), len(users_list)))
 
 
-def get_partition(users_list):
+def partition_groups(users_list):
     incidence = get_incidence_matrix(users_list)
     laplacian = incidence.transpose()*incidence
     w, v = eigs(laplacian)
@@ -67,6 +88,33 @@ def get_partition(users_list):
     return group1, group2
 
 
-get_partition(sorted(get_users()))
+def partition_bots(users_list):
+    global count
+    api = ShillSearchAPI.create_API()
+    humans = []
+    bots = []
+    for user in users_list:
+        count += 1
+        if count % 100 == 0:
+            print(count)
+        try:
+            metadata = api.get_metadata(user, True, True)
+            if classify(get_record_from_dict(metadata)[0:8]):
+                bots.append(user)
+            else:
+                humans.append(user)
+        except tweepy.error.TweepError as tweep:
+            pass
+    return humans, bots
 
 
+users = get_users(["Trump"])
+print(len(users), users[0])
+
+count = 0
+
+group1, group2 = partition_groups(sorted(users))
+
+group1h, group1b = partition_bots(group1)
+group2h, group2b = partition_bots(group2)
+print(len(group1h), len(group1b), len(group2h), len(group2b))
