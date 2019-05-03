c2ae6d2068e9dce2a2400f32da16da8c9381681f
Daniel O'Connor <DanielOC004@gmail.com>
2019-05-04 00:20:06 +0100

    Add function to create all databases required

---
+++ metrics/create_databases.py
@@ -0,0 +1,76 @@
+import psycopg2
+import consts
+
+connection = psycopg2.connect(**consts.db_creds)
+
+
+def create_dbs():
+    """
+    Creates the databases for storing user data.
+    :return:
+
+    """
+    cursor = connection.cursor()
+    create_table_1 = ''' CREATE TABLE user_metadata
+                         (usr_id VARCHAR(22) PRIMARY KEY,
+                          screen_name VARCHAR(55),
+                          no_statuses INTEGER,
+                          no_followers INTEGER,
+                          no_friends INTEGER,
+                          no_favourites INTEGER,
+                          no_listed INTEGER,
+                          default_profile BOOLEAN,
+                          geo_enabled BOOLEAN,
+                          custom_bg_img BOOLEAN,
+                          verified BOOLEAN,
+                          protected BOOLEAN); '''
+    create_table_2 = ''' CREATE TABLE user_bot_status
+                         (usr_id VARCHAR(22) PRIMARY KEY,
+                         is_bot BOOLEAN); '''
+    create_table_3 = ''' CREATE TABLE failed_users
+                         (usr_id VARCHAR(22) PRIMARY KEY,
+                          error_msg VARCHAR(200)); '''
+    create_table_4 = ''' CREATE TABLE interactions
+                        (id SERIAL PRIMARY KEY,
+                         usr VARCHAR(22),
+                         other_usr VARCHAR(22),
+                         interaction VARCHAR(22),
+                         time TIMESTAMP NOT NULL,
+                         topic_code VARCHAR(22)),
+                         twid VARCHAR (255); '''
+    create_table_5 = ''' CREATE TABLE topics
+                         (id SERIAL PRIMARY KEY,
+                          topic_code VARCHAR(22),
+                          hashtag VARCHAR(200)); '''
+    create_table_6 = ''' CREATE TABLE influences
+                         (id SERIAL PRIMARY KEY,
+                          usr VARCHAR(22),
+                          other_usr VARCHAR(22),
+                          UNIQUE (usr, other_usr)); '''
+    try:
+        cursor.execute(create_table_1)
+        cursor.execute("CREATE INDEX usr_id_index(usr_id)")
+        cursor.execute("CREATE INDEX screen_name_index(screen_name)")
+    except psycopg2.ProgrammingError:
+        connection.rollback()
+    try:
+        cursor.execute(create_table_2)
+    except psycopg2.ProgrammingError:
+        connection.rollback()
+    try:
+        cursor.execute(create_table_3)
+    except psycopg2.ProgrammingError:
+        connection.rollback()
+    try:
+        cursor.execute(create_table_4)
+    except psycopg2.ProgrammingError:
+        connection.rollback()
+    try:
+        cursor.execute(create_table_5)
+    except psycopg2.ProgrammingError:
+        connection.rollback()
+    try:
+        cursor.execute(create_table_6)
+    except psycopg2.ProgrammingError:
+        connection.rollback()
+    connection.commit()
